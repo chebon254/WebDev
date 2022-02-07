36fc4decfc8cb0f0a0626a0a9a8fb92007f8f95b
chebon254 <kelvinchebon90@gmail.com>
2022-02-07 16:07:32 +0300

    Webdev being redefined for live hosting and real client attraction

--- js/script.js
+++ js/script.js
@@ -1,31 +1,76 @@
 //Create A Responsive Menu Hide Show Side Menu
-const menu = document.querySelector(".links");
-const menuBtn = document.querySelector(".menu-bar");
+const header = document.querySelector(".header");
+const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const menu = document.querySelector(".links");
+// Onscroll Change Nav Bar Background
+window.onscroll = ()=>{
+    this.scrollY > 20 ? header.classList.add("sticky") : header.classList.remove("sticky");
+}
 
 // Onclick For Menu Bar
 menuBtn.onclick = ()=>{
     menu.classList.add("active");
-    menu.classList.remove("hide");
+    menuBtn.classList.add("hide");
 }
 
 // Onclick For Cancel Menu 
 cancelBtn.onclick = ()=>{
-    menu.classList.add("hide");
     menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
 }
 
-var loadTimer;
 
-function loadScreen() {
-  loadTimer = setTimeout(showPage, 2000);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showPage() {
-  document.getElementById("loading").style.display = "none";
-  document.getElementById("load").style.display = "block";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function redirect() {
-  location.replace("contact.html");
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonial");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  // /* == For dots == */
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("testimonial");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) 
+  {slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
